e74be29736032045dc97fa93bb7d19908f6f48ce
Daniel Ho <daniel.ho337@gmail.com>
2024-07-01 16:19:38 -0700

    I'm so doomed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,17 +1,17 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const usersRouter = require('./users.js');
-const groupsRouter = require('./groups.js');
-const venuesRouter = require('./venues.js');
-const eventsRouter = require('./events.js');
-const membersRouter = require('./members.js');
+// const usersRouter = require('./users.js');
+// const groupsRouter = require('./groups.js');
+// const venuesRouter = require('./venues.js');
+// const eventsRouter = require('./events.js');
+// const membersRouter = require('./members.js');
 const { restoreUser } = require("../../utils/auth.js");
 
-router.use('/users', usersRouter);
-router.use('/groups', groupsRouter);
-router.use('/venues', venuesRouter);
-router.use('/events', eventsRouter);
-router.use('/members', membersRouter);
+// router.use('/users', usersRouter);
+// router.use('/groups', groupsRouter);
+// router.use('/venues', venuesRouter);
+// router.use('/events', eventsRouter);
+// router.use('/members', membersRouter);
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
