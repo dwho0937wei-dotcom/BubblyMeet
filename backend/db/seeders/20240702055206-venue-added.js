fccb9d9ab8a61e5f764e961bada7e226369a49cb
Daniel Ho <daniel.ho337@gmail.com>
2024-07-01 22:57:41 -0700

    venue added

---
+++ backend/db/seeders/20240702055206-venue-added.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+options.tableName = "Venues";
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert(options, [
+      {
+        "groupId": 1,
+        "address": "123 Disney Lane",
+        "city": "New York",
+        "state": "NY",
+        "lat": 37.7645358,
+        "lng": -122.4730327
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(options, {
+      "groupId": 1,
+      "address": "123 Disney Lane",
+      "city": "New York",
+      "state": "NY",
+      "lat": 37.7645358,
+      "lng": -122.4730327
+    });
+  }
+};
