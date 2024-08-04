bb9a2afeff1237b1a754cc565c9747f582ac71f4
Daniel Ho <daniel.ho337@gmail.com>
2024-08-04 15:24:31 -0700

    Fixing the event type to be "In person", not "In Person".

--- backend/db/migrations/20240702015028-create-event.js
+++ backend/db/migrations/20240702015028-create-event.js
@@ -39,7 +39,7 @@ module.exports = {
         allowNull: false
       },
       type: {
-        type: Sequelize.ENUM('Online', 'In Person'),
+        type: Sequelize.ENUM('Online', 'In person'),
         allowNull: false
       },
       capacity: {
