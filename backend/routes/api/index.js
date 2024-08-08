37a4639370e221b97b1b3633acc5349ce8cd9399
Daniel Ho <daniel.ho337@gmail.com>
2024-08-08 15:51:21 -0700

    I personally believe that we should restoreUser, i.e. get the current User info before starting any routes that may require that info.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,6 +9,11 @@ const groupImagesRouter = require('./group-images.js');
 const eventImagesRouter = require('./event-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/groups', groupsRouter);
@@ -16,11 +21,6 @@ router.use('/venues', venuesRouter);
 router.use('/events', eventsRouter);
 router.use('/group-images', groupImagesRouter);
 router.use('/event-images', eventImagesRouter);
-
-// Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-router.use(restoreUser);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
