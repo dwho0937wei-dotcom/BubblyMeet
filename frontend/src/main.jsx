f02e7beec59937b78d8e5d9975fb62a8763fd6af
Daniel Ho <daniel.ho337@gmail.com>
2024-08-24 12:13:02 -0700

    Supposedly completed attaching the CSRF Token to the Frontend including its index.html, build folder, and other index.html at all other routes NOT starting with /api.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
@@ -16,5 +17,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 );
 
 if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
