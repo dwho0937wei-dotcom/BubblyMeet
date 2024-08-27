77b6f25a8c178d32b285b5afd4c70da15e0a7d9a
Daniel Ho <daniel.ho337@gmail.com>
2024-08-26 17:33:22 -0700

    In progress of Phase 4, just finished wrapping the App with the ModalProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
+import { ModalProvider } from './context/Modal';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
@@ -11,9 +12,11 @@ const store = configureStore();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
 
