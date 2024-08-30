7e6a01f8ac3cf287b834bc2299e10263d83afd6d
Daniel Ho <daniel.ho337@gmail.com>
2024-08-29 21:00:27 -0700

    Forgot the submit button. Put that in the CreateEventFormPage as well. By the way, the previous commit is about setting up the rest of the visual features in CreateEventFormPage, not EventDetailsPage.

--- frontend/src/components/CreateEventFormPage/CreateEventFormPage.jsx
+++ frontend/src/components/CreateEventFormPage/CreateEventFormPage.jsx
@@ -32,114 +32,124 @@ function CreateEventFormPage() {
         }
     }, [group, setIsLoaded])
 
+    //! Event for submitting
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("You've clicked the button!");
+    } 
+
     return (
         <>
             <h1>Create an event for {isLoaded && group.name}</h1>
 
-            {/* Event Name */}
-            <p>What is the name of the event?</p>
-            <input
-                id="name"
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="What is your group name?"
-            />
-            <div className="errors">
-                {errors.name}
-            </div>
-            <h1>-------------------------------------------</h1>
+            <form onSubmit={handleSubmit}>
+                {/* Event Name */}
+                <p>What is the name of the event?</p>
+                <input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="What is your group name?"
+                />
+                <div className="errors">
+                    {errors.name}
+                </div>
+                <h1>-------------------------------------------</h1>
 
-            {/* Type, Visibility, & Price */}
-            <p>Is this an in person or online group?</p>
-            <select 
-                name="type" 
-                value={type}
-                onChange={(e) => setType(e.target.value)}
-                required 
-            >
-                <option value="Unselected" disabled hidden>(select one)</option>
-                <option value="In person">In person</option>
-                <option value="Online">Online</option>
-            </select>
-            <div className="errors">
-                {errors.type}
-            </div>
+                {/* Type, Visibility, & Price */}
+                <p>Is this an in person or online group?</p>
+                <select 
+                    name="type" 
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    required 
+                >
+                    <option value="Unselected" disabled hidden>(select one)</option>
+                    <option value="In person">In person</option>
+                    <option value="Online">Online</option>
+                </select>
+                <div className="errors">
+                    {errors.type}
+                </div>
 
-            <p>Is this group private or public?</p>
-            <select 
-                name="private" 
-                value={privacy} 
-                onChange={(e) => setPrivacy(e.target.value)}
-                required
-            >
-                <option value="Unselected" disabled hidden>(select one)</option>
-                <option value={true}>Private</option>
-                <option value={false}>Public</option>
-            </select>
-            <div className="errors">
-                {errors.private}
-            </div>
+                <p>Is this group private or public?</p>
+                <select 
+                    name="private" 
+                    value={privacy} 
+                    onChange={(e) => setPrivacy(e.target.value)}
+                    required
+                >
+                    <option value="Unselected" disabled hidden>(select one)</option>
+                    <option value={true}>Private</option>
+                    <option value={false}>Public</option>
+                </select>
+                <div className="errors">
+                    {errors.private}
+                </div>
 
-            <p>What is the price for your event?</p>
-            <input
-                id="name"
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                placeholder="0"
-            />
-            <div className="errors">
-                {errors.price}
-            </div>
-            <h1>-------------------------------------------</h1>
+                <p>What is the price for your event?</p>
+                <input
+                    id="name"
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    placeholder="0"
+                />
+                <div className="errors">
+                    {errors.price}
+                </div>
+                <h1>-------------------------------------------</h1>
 
-            <p>When does your event start?</p>
-            <input
-                id="name"
-                type="datetime-local"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-            />
-            <div className="errors">
-                {errors.startDate}
-            </div>
+                <p>When does your event start?</p>
+                <input
+                    id="name"
+                    type="datetime-local"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                />
+                <div className="errors">
+                    {errors.startDate}
+                </div>
 
-            <p>When does your event end?</p>
-            <input
-                id="name"
-                type="datetime-local"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-            />
-            <div className="errors">
-                {errors.endDate}
-            </div>
-            <h1>-------------------------------------------</h1>
+                <p>When does your event end?</p>
+                <input
+                    id="name"
+                    type="datetime-local"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                />
+                <div className="errors">
+                    {errors.endDate}
+                </div>
+                <h1>-------------------------------------------</h1>
 
-            <p>Please add an image url for your group below:</p>
-            <input 
-                id="imageUrl"
-                type="url" 
-                value={imageUrl}
-                onChange={(e) => setImageUrl(e.target.value)}
-                placeholder="Image Url"
-            />
-            <div className="errors">
-                {errors.imageUrl}
-            </div>
-            <h1>-------------------------------------------</h1>
+                <p>Please add an image url for your group below:</p>
+                <input 
+                    id="imageUrl"
+                    type="url" 
+                    value={imageUrl}
+                    onChange={(e) => setImageUrl(e.target.value)}
+                    placeholder="Image Url"
+                />
+                <div className="errors">
+                    {errors.imageUrl}
+                </div>
+                <h1>-------------------------------------------</h1>
 
-            <p>Please describe your event:</p>
-            <textarea 
-                id="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="Please include at least 30 characters"
-            />
-            <div className="errors">
-                {errors.description}
-            </div>
+                <p>Please describe your event:</p>
+                <textarea 
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Please include at least 30 characters"
+                />
+                <div className="errors">
+                    {errors.description}
+                </div>
+
+                <button>Create Event</button>
+            </form>
         </>
     )
 }
